826176156f08c6105a23159dc0da6253ca1c9cae
ivanvr <github@ivanyvenian.com>
2013-04-23 15:17:24 -0500

    And we have a CLI! This would've been painful a few years ago.

--- README.md
+++ README.md
@@ -6,10 +6,26 @@ In the past I used LastPass' generator, but typing those passwords in a phone is
 
 Hope someone finds this useful and let me know what stupid security mistakes I've made :)
 
+## Requirements
+
+Probably just on *nix for now.
+
+Needs the `xsel` program to copy passphrases to the clipboard.
+
 ## Usage
 
     $ gem install ruby-dice
-    $ dice --help
+    $ dice help throw
+
+## Custom wordlists
+
+You can get additional wordlists from:
+
+    http://world.std.com/~reinhold/diceware.html#languages
+
+Save them to `~/.wordlists`. Say you save one to `~/.wordlists/custom.txt`, then to use:
+
+    $ dice -w custom
 
 ## To do
 
---
+++ lib/ruby-dice/cli.rb
@@ -0,0 +1,22 @@
+require 'thor'
+require 'ruby-dice'
+
+module RubyDice
+  class CLI < Thor
+    desc 'throw', 'Generate and copy a passphrase'
+    option :length,    type: :numeric, default: 5,     aliases: :l, desc: 'Amount of words to use'
+    option :print,     type: :boolean, default: false, aliases: :p, desc: 'Output passphrase to terminal'
+    option :wordlist,  type: :string,  default: nil,   aliases: :w, desc: 'Use a custom wordlist file', banner: 'filename'
+    def throw
+      passphrase_options = {}.tap do |o|
+        o[:words] = options['length']
+        o[:wordlist] = options['wordlist']
+      end
+
+      passphrase = RubyDice::Passphrase.generate passphrase_options
+      IO.popen('xsel --clipboard --input', 'r+') { |c| c.print passphrase }
+      puts passphrase if options['print']
+    end
+    default_task :throw
+  end
+end
